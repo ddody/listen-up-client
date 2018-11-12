bc958c0d660eebd6ca3b3f31b7bb65ad794a7204
ddody <laccooon@gmail.com>
2018-11-12 20:32:58 +0900

    edit base url

--- src/url/base-url.js
+++ src/url/base-url.js
@@ -1,11 +1,10 @@
 let BASE_URL;
 console.log(process.env);
+
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = `https://api-dev.listenup.kr`;
 } else if (process.env.NODE_ENV === 'production') {
   BASE_URL = `https://api.listenup.kr`;
-} else if (process.env.NODE_ENV === 'local') {
-  BASE_URL = `http://localhost:8081`;
 }
 
 export default BASE_URL;
